6f0caa1b39db0b23ce4bee286baacdb1a8285716
Jeongyeon Park <ririsid@me.com>
2024-06-19 17:05:46 +0900

    Initial Commit

---
+++ Tests/PexelsTests/PexelsTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Pexels
+
+final class PexelsTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
